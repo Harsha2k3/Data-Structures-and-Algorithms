223f1de8eeac85ba79b3d405850641b3cb1c1a06
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-10-01 12:34:13 +0530

    Time: 914 ms (21.64%), Space: 20.6 MB (97.19%) - LeetHub

---
+++ 0835-linked-list-components/0835-linked-list-components.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+
+        temp = head
+        count = 0
+
+        connected = False  
+
+        while(temp):
+            if(temp.val in nums):
+                if(connected == False):
+                    connected = True
+                    count += 1
+            else:
+                connected = False
+
+            temp = temp.next
+
+        return count
