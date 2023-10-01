6ecf46d208ca2f888db06ab721b0907ca4ff6e96
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-10-01 12:34:45 +0530

    Time: 230 ms (30.84%), Space: 29.5 MB (76.87%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -0,0 +1,24 @@
+# Dynamic Sized Sliding Window (Length of Sub-array is not given)
+
+class Solution:
+    def minSubArrayLen(self, t: int, nums: List[int]) -> int:
+
+        start = 0
+        end = 0
+        curr_sum = nums[start]
+
+        min_length = float("inf")
+
+        while(end < len(nums)):
+            if(curr_sum >= t):
+                min_length = min(min_length, end - start + 1)
+                curr_sum -= nums[start]
+                start += 1
+            else:
+                end += 1
+                if end < len(nums):
+                    curr_sum += nums[end]
+
+        if(min_length == float("inf")):
+            return 0
+        return min_length
