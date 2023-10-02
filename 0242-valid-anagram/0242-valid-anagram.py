f6e4ee2794d6e86e31d6ba4045f9f6aa76bf964e
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-10-02 16:31:55 +0530

    Time: 51 ms (73.06%), Space: 16.8 MB (78.93%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        a = Counter(s)
+        b = Counter(t)
+
+        if(a == b):
+            return True
+        else:
+            False
