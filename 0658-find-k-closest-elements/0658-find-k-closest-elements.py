3b97731b7cb46d56763bfd6ab946704c4fdd972d
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-10-02 20:55:30 +0530

    Time: 305 ms (19.98%), Space: 17.9 MB (64.63%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -5,10 +5,15 @@ class Solution:
     
         # Iterate through the remaining elements
         for i in range(k, len(arr)):
-            # Check if the current element is closer to x than the edge element of the window
+            # Check if the current element(element that is enetering the window) is closer to x than the left edge element of the window
             if abs(arr[i] - x) < abs(window[0] - x) or (abs(arr[i] - x) == abs(window[0] - x) and arr[i] < window[0]):
                 # Update the sliding window
                 window.pop(0)
                 window.append(arr[i])
         
         return window
+
+        # We know that the sub-array should be contiguous of size k
+        # So, use fixed sized sliding window of size k
+        # As any how the array is sorted, we can grab k closest 
+        # elements by just checking if the current element is closer to x than the edge element of the window and if the condition satisfies, just move the window
