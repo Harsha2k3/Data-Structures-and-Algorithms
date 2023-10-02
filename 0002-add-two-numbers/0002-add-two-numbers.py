253a82dbd644cb7c5f03ee54d277325dfb38fc3b
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-10-02 16:39:07 +0530

    Time: 78 ms (5.65%), Space: 16.4 MB (52.27%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -0,0 +1,23 @@
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+
+        dummy = ListNode(0)
+        dummy_ = dummy
+
+        num1 = num2 = ""
+
+        while l1:
+            num1 += str(l1.val)
+            l1 = l1.next
+
+        while l2:
+            num2 += str(l2.val)
+            l2 = l2.next
+
+        res = str(int(num1[::-1]) + int(num2[::-1]))[::-1]
+        
+        for i in res:
+            dummy.next = ListNode(i)
+            dummy = dummy.next
+
+        return dummy_.next    
