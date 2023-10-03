0d8c490309e11a24c8906506afe48019664b085a
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-10-03 10:46:29 +0530

    Time: 36 ms (73.49%), Space: 16.2 MB (57.31%) - LeetHub

--- 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.py
+++ 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.py
@@ -4,10 +4,10 @@ class Solution:
         sub_arr = str(num)[:k]
         count = 0
 
-        # if(num % int(sub_arr) == 0):
-        #     count += 1
+        if(num % int(sub_arr) == 0):
+            count += 1
 
-        for i in range(len(str(num))-k+1):
+        for i in range(1,len(str(num))-k+1):
             sub_arr = str(num)[i:k+i]
 
             if(int(sub_arr) != 0 and num % int(sub_arr) == 0):
