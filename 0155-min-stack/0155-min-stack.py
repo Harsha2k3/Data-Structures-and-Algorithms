fad6457430ea4eff4a9c6df48aad84bd4d35f8f3
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-10-06 21:54:54 +0530

    Time: 451 ms (9.35%), Space: 20 MB (94.39%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,49 +1,19 @@
-# class MinStack:
-
-#     def __init__(self):
-#         self.list = []
-
-#     def push(self, val: int) -> None:
-#         self.list.append(val)
-
-#     def pop(self) -> None:
-#         self.list.pop()
-
-#     def top(self) -> int:
-#         return self.list[len(self.list)-1]
-
-#     def getMin(self) -> int:
-#         return min(self.list)   ==> This will take O(n)
-
-
-
-# But we need to do it in constant time (O(1))
-
-
 class MinStack:
 
     def __init__(self):
-        self.stack = []
-        self.minstack = []
+        self.list = []
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        if(self.minstack):
-            self.minstack.append(min(val,self.minstack[-1]))
-        else:
-            self.minstack.append(val)
-        
+        self.list.append(val)
 
     def pop(self) -> None:
-        self.stack.pop()
-        self.minstack.pop()
+        self.list.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.list[len(self.list)-1]
 
     def getMin(self) -> int:
-        return self.minstack[-1]
-        
+        return min(self.list)
 
 
 # Your MinStack object will be instantiated and called as such:
