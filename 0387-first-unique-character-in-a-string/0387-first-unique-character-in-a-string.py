18d732a4787bc6bee0f20dff77513621f91d8f02
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-10-16 23:44:21 +0530

    Time: 246 ms (17.95%), Space: 16.8 MB (6.11%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,8 +1,23 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
 
+        # Maintain a list(q here)
+        # Just traverse along the string and just append the
+        # current character to the list if it is not in the list 
+        # If char is already present in the list, just 
+        # remove it from the list
+        # If we have done till this, it will handle 
+        # only situations where a char is repeated 
+        # 2 times
+        # But if it is repeated more than 2 times it'll not work
+        # So, just when ever the char appears for the 2nd
+        # time, just append it to a seperate list
+        # So, append the char to list when it's not there in the new list too.
+
+
+
         l = [i for i in s]
-        q = []              # Queue
+        q = []              
         l_ = []
 
         for i in l:
