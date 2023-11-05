be5423d0f011b0fb26fd8a77e842c5fd2f414898
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-11-05 22:50:05 +0530

    Time: 95 ms (31.12%), Space: 16.3 MB (70.14%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -14,3 +14,8 @@ class Solution:
                 count += 1
 
         return count == n
+
+        # Just sort them
+        # Now, xor all the integers in nums
+        # xor will be not be 0, when the pair ends and new pair starts
+        # Just simply count the cases where xor is not 0
