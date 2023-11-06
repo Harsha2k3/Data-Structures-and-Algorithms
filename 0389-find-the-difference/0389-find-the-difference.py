1e94e3ebb44b7ddfcd2e5c2c7827bc22dbd72267
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-11-06 20:39:04 +0530

    Time: 35 ms (88.66%), Space: 16.2 MB (52.91%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -10,3 +10,7 @@ class Solution:
             xor ^= ord(i)
 
         return chr(xor)
+
+        # Get all the ascii values of the char's in s and t
+        # Apply XOR between all of them
+        # The remaining ascii value's char is the result that is required
