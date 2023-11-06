9ee1cb15856d9bedab410e807d07e8490fd4f7ea
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-11-06 19:15:18 +0530

    Time: 34 ms (91.96%), Space: 16.2 MB (52.91%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        
+        xor = 0
+
+        for i in s:
+            xor ^= ord(i)
+        
+        for i in t:
+            xor ^= ord(i)
+
+        return chr(xor)
