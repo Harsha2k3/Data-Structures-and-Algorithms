7a6e09c9f5d8cb5f6e6ab8bf3a1c428ee20d98e5
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2023-11-10 20:07:57 +0530

    Time: 42 ms (97.27%), Space: 16.3 MB (38.26%) - LeetHub

--- 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
@@ -1,5 +1,14 @@
 class Solution:
     def targetIndices(self, array: List[int], val: int) -> List[int]:
+
+        # Applynormal binary search
+        # But for detecting multiple elements,
+        # Pop it from the array and start everything from starting
+        # For that, just re-set start,end and mid pointers
+        # Also as we are popping the items, keep a track of how many elements are popped
+        # So, that we can add it to the index 
+        # Because after popping, the elements in array decreases by 1 each time
+        # So while pushing the index of target to the resultant array, we need to add the number of popped items till that point to get correct index
         
         array.sort()
 
@@ -22,7 +31,7 @@ class Solution:
             if(array[mid] == val):
                 l.append(array.index(array[mid]) + p)
                 array.pop(mid)
-                p += 1
+                p += 1        # Number of pop's
                 start = 0
                 end = len(array) - 1
                 mid = (start + end) // 2
