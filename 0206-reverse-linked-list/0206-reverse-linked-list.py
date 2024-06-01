3e32a4f410bf7e4af5913dfab7efbbdb9bf862de
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-06-01 20:34:09 +0530

    Time: 31 ms (90.14%), Space: 17.6 MB (96.22%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -1,3 +1,25 @@
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+#         prev = None
+#         temp = head
+
+#         def rec():
+
+#             nonlocal temp, prev
+
+#             if(not temp):
+#                 return prev
+
+#             else:
+#                 nxt = temp.next
+#                 temp.next = prev
+#                 prev = temp
+#                 temp = nxt
+#                 return rec()
+
+#         return rec()
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
@@ -9,7 +31,7 @@ class Solution:
             temp.next = prev
             prev = temp
             temp = nxt
-
+        
         head = prev
 
         return head
