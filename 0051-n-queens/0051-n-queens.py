47ff76e0c5a053ee639299c45f7269196f66d5ee
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-06-15 21:21:20 +0530

    Time: 78 ms (25.47%), Space: 16.9 MB (91.06%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,62 @@
+class Solution:
+
+    # https://haseebq.com/n-queens-visualizer/
+
+    def isSafe1(self, row, col, board, n):
+
+        # check upper element
+        duprow = row
+        dupcol = col
+
+        while row >= 0 and col >= 0:
+            if board[row][col] == 'Q':
+                return False
+            row -= 1
+            col -= 1
+
+
+        col = dupcol
+        row = duprow
+
+        while col >= 0:
+            if board[row][col] == 'Q':
+                return False
+            col -= 1
+
+
+        row = duprow
+        col = dupcol
+        
+        while row < n and col >= 0:
+            if board[row][col] == 'Q':
+                return False
+            row += 1
+            col -= 1
+
+
+        return True
+
+
+    def solve(self, col, board, res, n):
+
+        if col == n:
+            res.append(list(board))
+            return
+
+
+        for row in range(n):
+            if self.isSafe1(row , col , board , n):
+                board[row] = board[row][:col] + 'Q' + board[row][col+1:]
+                self.solve(col+1 , board , res , n)
+                board[row] = board[row][:col] + '.' + board[row][col+1:]
+
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+
+        res = []
+
+        board = ['.' * n for i in range(n)]
+
+        self.solve(0, board, res, n)
+
+        return res
