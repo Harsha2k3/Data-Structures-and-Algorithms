3e43abc11cb14c0fa63c29999c5ea9a66c4a5504
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-06-19 19:58:44 +0530

    Time: 61 ms (33.99%), Space: 17.2 MB (7.76%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        # T.C = n! x n  (Combinations ==> n! and for loop ==> n)
+        # S.C = O(n) auxiliary S.C for recursion
+
+        res = []
+
+        def rec(ind , nums):
+
+            if ind > len(nums) - 1:
+                res.append(nums.copy())
+                return
+
+            for i in range(ind , len(nums)):
+                if i > ind and nums[i] == nums[i - 1]:
+                    continue
+                
+                nums[ind] , nums[i] = nums[i] , nums[ind]
+                rec(ind + 1 , nums)
+                nums[ind] , nums[i] = nums[i] , nums[ind]
+
+        nums.sort()
+        rec(0 , nums)
+
+        res_final = set()
+
+        for i in res:
+            res_final.add(tuple(i))
+
+        return list(res_final)
