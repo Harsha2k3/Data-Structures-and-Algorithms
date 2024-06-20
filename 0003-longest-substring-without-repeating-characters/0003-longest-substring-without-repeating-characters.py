d2086a1896bada4b688d4fd2ff456599c8a1ae5f
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-06-20 20:44:52 +0530

    Time: 54 ms (64.28%), Space: 16.6 MB (44.37%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -2,27 +2,50 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
         start = 0
-        end = 0
-        max_length = 0
-        seen = set()               # Empty Set
+        max_le = 0
 
-        while end < len(s):
-            if s[end] not in seen:
+        seen = set()
+
+        for end in range(len(s)):
+
+            if(s[end] not in seen):
                 seen.add(s[end])
-                max_length = max(max_length, end - start + 1)
-                end += 1
+                
+
             else:
-                seen.remove(s[start])
-                start += 1
+                while s[end] in seen:
+                    seen.remove(s[start])
+                    start += 1
+                seen.add(s[end])
 
-        return max_length
+            max_le = max(max_le , end - start + 1)
+
+        return max_le
 
 
 
-        # Here say, current sliding window is abc
-        # Now for checking and moving sliding window to make abca 
-        # No need to check for bc, just check s[end] i.e, a is in 
-        # seen set or not, if it is in seen set then start removing
-        # the letters from back side until the letter a is removed
-        # Now, bca is current sliding window
-        # Continue this.
+        # start = 0
+        # end = 0
+        # max_length = 0
+        # seen = set()               # Empty Set
+
+        # while end < len(s):
+        #     if s[end] not in seen:
+        #         seen.add(s[end])
+        #         max_length = max(max_length, end - start + 1)
+        #         end += 1
+        #     else:
+        #         seen.remove(s[start])
+        #         start += 1
+
+        # return max_length
+
+
+
+        # # Here say, current sliding window is abc
+        # # Now for checking and moving sliding window to make abca 
+        # # No need to check for bc, just check s[end] i.e, a is in 
+        # # seen set or not, if it is in seen set then start removing
+        # # the letters from back side until the letter a is removed
+        # # Now, bca is current sliding window
+        # # Continue this.
