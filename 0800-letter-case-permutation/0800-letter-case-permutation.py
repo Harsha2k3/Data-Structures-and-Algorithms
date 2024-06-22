006ff1e12b796a49270357c020decbff176014c7
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-06-22 16:25:13 +0530

    Time: 38 ms (49.46%), Space: 13.4 MB (32.26%) - LeetHub

---
+++ 0800-letter-case-permutation/0800-letter-case-permutation.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def letterCasePermutation(self, s):
+        res = []
+
+        def rec(ind, l):
+            if ind == len(s):
+                res.append("".join(l))
+                return
+
+            if l[ind].isdigit():
+                rec(ind + 1, l)
+            else:
+                # Recur without changing the case
+                rec(ind + 1, l)
+
+                # Recur after changing the case
+                l[ind] = l[ind].upper() if l[ind].islower() else l[ind].lower()
+                rec(ind + 1, l)
+                
+                # Revert the change for backtracking
+                l[ind] = l[ind].upper() if l[ind].islower() else l[ind].lower()
+
+        rec(0, list(s))
+        return res
