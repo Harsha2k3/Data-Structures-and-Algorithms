10b10a3ad353c0439be9a678a6bae7fd37a5231d
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-06-25 14:33:52 +0530

    Time: 12 ms (74.68%), Space: 11.7 MB (15.47%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -1,15 +1,44 @@
 class Solution(object):
     def postorderTraversal(self, root):
 
-        def postorder(root , arr):
+        # def postorder(root , arr):
 
-            if not root:
-                return
+        #     if not root:
+        #         return
 
-            postorder(root.left , arr)
-            postorder(root.right , arr)
-            arr.append(root.val)
+        #     postorder(root.left , arr)
+        #     postorder(root.right , arr)
+        #     arr.append(root.val)
 
-            return arr
+        #     return arr
 
-        return postorder(root , [])
+        # return postorder(root , [])
+
+        # # (Or)
+
+        postorder = []
+
+        if root is None:
+            return postorder
+
+        st1, st2 = [], []
+
+        st1.append(root)
+
+        while st1:
+
+            root = st1.pop()
+
+            st2.append(root)
+
+            if root.left is not None:
+                st1.append(root.left)
+
+            if root.right is not None:
+                st1.append(root.right)
+
+        while st2:
+            postorder.append(st2[-1].val)
+            st2.pop()
+
+        return postorder
