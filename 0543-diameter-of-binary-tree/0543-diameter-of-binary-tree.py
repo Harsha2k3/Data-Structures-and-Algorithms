3d29aa063f59648d963a3f29e0d06b9ae448d1dc
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-06-25 19:32:00 +0530

    Time: 42 ms (86.96%), Space: 19.2 MB (79.59%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -1,6 +1,14 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
 
+        # Diameter ==> Longest path between any 2 nodes
+        # Same as 104. Maximum Depth of B.T with some changes
+        # Just while checking height keep a track of lh + rh
+        # At every node we can get lh and rh
+        # We keep a track of the distance between
+        # left most node of lst and right most node of rst of a node
+
+
         lp = 0
 
         def rec(root):
