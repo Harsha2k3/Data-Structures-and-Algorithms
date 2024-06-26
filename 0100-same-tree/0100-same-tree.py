dd6cbbb6c33684a1ce03751398b004e1c1985ff6
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-06-26 13:26:53 +0530

    Time: 34 ms (66.03%), Space: 16.3 MB (98.27%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,8 +1,6 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-
-        preorder = []
-
+        
         def rec(root1 , root2):
 
             if not root1 and not root2:
@@ -13,6 +11,5 @@ class Solution:
 
             # Simultaneously we have to travserse the trees
             return root1.val == root2.val and rec(root1.left , root2.left) and rec(root1.right , root2.right)
-
 
         return rec(p , q)   
