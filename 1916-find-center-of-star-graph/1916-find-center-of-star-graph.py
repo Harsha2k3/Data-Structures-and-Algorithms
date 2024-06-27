ea44015fed2e5b77cf5506c371bad7c2eef7aa8d
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-06-27 13:10:19 +0530

    Time: 660 ms (55.75%), Space: 52.4 MB (35.24%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.py
@@ -1,12 +1,16 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
 
-        element_count = defaultdict(int)
+        # element_count = defaultdict(int)
 
-        for edge in edges:
-            for element in edge:
-                element_count[element] += 1
+        # for i , j in edges:
+        #     element_count[i] += 1
+        #     element_count[j] += 1
         
-        common_elements = [element for element, count in element_count.items() if count == len(edges)]
+        # common_elements = [element for element, count in element_count.items() if count == len(edges)]
         
-        return common_elements[0]
+        # return common_elements[0]
+
+        # (Or)
+        
+        return edges[0][0] if edges[0][0] in edges[1] else edges[0][1]
