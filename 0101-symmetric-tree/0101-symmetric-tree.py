995052da9842c0b4a801045b3bcaf604937c4023
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-06-29 19:39:02 +0530

    Time: 31 ms (89.79%), Space: 16.5 MB (83.13%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -1,11 +1,14 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 
+        # https://www.youtube.com/watch?v=nKggNAiEpBE&list=PLkjdNRgDmcc0Pom5erUBU4ZayeU9AyRRu&index=26
+        # Watch from 2:24 to 6:00
+
         root1 , root2 = root.left , root.right
 
         def rec(root1 , root2):
 
-            if (not root1) or (not root2):
+            if not root1 or not root2:
                 return root1 == root2
 
             if root1.val != root2.val:
@@ -25,5 +28,6 @@ class Solution:
             # return False
 
             return rec(root1.left , root2.right) and rec(root1.right , root2.left)
+            # rec(root1.right , root2.left) ==> Backtracking
 
         return not root or rec(root1 , root2)
