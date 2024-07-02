9462ab318d83fde9b13c7ab9274dca6a3ae8cbb1
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-07-02 12:33:57 +0530

    Time: 372 ms (54.16%), Space: 64.6 MB (23.24%) - LeetHub

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,47 @@
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], t: int) -> int:
+
+        pm = defaultdict(int)  # Parent Map
+
+        vis = set()   # Visited
+
+        max_time = 0
+
+        target = None
+
+        def getParent(root , parent , t):
+
+            nonlocal target
+            
+            if not root:
+                return
+
+            # Getting the node whose value is t
+            if root.val == t:
+                target = root     
+
+            pm[root] = parent
+            getParent(root.left , root, t)
+            getParent(root.right , root, t)
+
+        def dfs(node , time):
+            
+            nonlocal max_time
+            
+            if not node or node in vis:
+                return
+
+            vis.add(node)
+            
+            max_time = max(max_time , time)
+            
+            dfs(node.left , time + 1)
+            dfs(node.right , time + 1)
+            dfs(pm[node] , time + 1)
+
+        getParent(root , None , t)
+        
+        if target:
+            dfs(target , 0)
+
+        return max_time        
