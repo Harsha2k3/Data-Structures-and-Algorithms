cd44d1cea5d216e6ae5a8156de6896aea981cbbb
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-07-04 18:27:20 +0530

    Time: 16 ms (45.11%), Space: 11.8 MB (14.95%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -15,25 +15,64 @@ class Solution(object):
         # return preorder(root , [])
 
 
+        # # (Or)
+
+
+        # preorder = []
+        
+        # if root is None:
+        #     return preorder
+        
+        # stack = []
+        # stack.append(root)
+        
+        # while stack:
+
+        #     root = stack.pop()
+
+        #     preorder.append(root.val)
+
+        #     if root.right:
+        #         stack.append(root.right)
+
+        #     if root.left:
+        #         stack.append(root.left)
+
+        # return preorder
+
+
+
+        # Same as Morris Inorder 
+        # Just keep preorder.append(curr.val) in "if not prev.right:"
+        # Because, as it's root-left-right we have to take
+        # the node when we are making the connection
+
+
 
         preorder = []
+
+        curr = root
+
+        while curr:
+
+            if not curr.left:
+                preorder.append(curr.val)
+                curr = curr.right
+
+            else:
+
+                prev = curr.left
+
+                while(prev.right and prev.right != curr):
+                    prev = prev.right
+                
+                if not prev.right:
+                    prev.right = curr
+                    preorder.append(curr.val)
+                    curr = curr.left
+
+                else:
+                    prev.right = None
+                    curr = curr.right
         
-        if root is None:
-            return preorder
-        
-        stack = []
-        stack.append(root)
-        
-        while stack:
-
-            root = stack.pop()
-
-            preorder.append(root.val)
-
-            if root.right:
-                stack.append(root.right)
-
-            if root.left:
-                stack.append(root.left)
-
         return preorder
