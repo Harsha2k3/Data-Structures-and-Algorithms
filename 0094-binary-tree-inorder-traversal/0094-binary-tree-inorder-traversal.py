7ddf577915faaac94b1562cc70e61823fa46247b
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-07-04 18:16:22 +0530

    Time: 20 ms (13.84%), Space: 11.6 MB (76.93%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -1,5 +1,57 @@
-class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+class Solution(object):
+    def inorderTraversal(self, root):
+
+        # def inorder(root , arr):
+
+        #     if not root:
+        #         return
+
+        #     inorder(root.left , arr)
+        #     arr.append(root.val)
+        #     inorder(root.right , arr)
+
+        #     return arr
+
+        # return inorder(root , [])
+
+
+
+        # inorder = []
+
+        # if not root:
+        #     return inorder
+
+        # stack = []
+
+        # while root or stack:
+
+        #     while root:
+        #         stack.append(root)
+        #         root = root.left
+
+        #     root = stack.pop()
+        #     inorder.append(root.val)
+        #     root = root.right
+
+        # return inorder
+
+
+
+
+
+        # Inorder using Recrsion ==> O(n) auxiliary space
+        # Inorder using Stack    ==> O(n) space for stack
+        # Morris Inorder         ==> O(1) space
+        # Every time we go to left subtree of current and then we will go to it's complete right
+        # Once we reach right node of the left subtree,
+        # We will make (that node).right = current node
+        # So that we can go to the current node i.e, 
+        # We can go to the root of that sub-tree directly
+        # without backtracking
+        # While we visit (that node) again make sure we 
+        # that the connection is removed
+        # So, (that node).right = None
+
 
         inorder = []
 
