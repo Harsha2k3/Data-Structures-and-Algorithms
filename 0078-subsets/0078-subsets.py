0fbf03b05fa8cdddf1be70f4eed0a8abbeb1a69f
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-07-23 19:00:28 +0530

    Time: 38 ms (50.35%), Space: 16.7 MB (49.48%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
 
+        # Take or not take pattern
+
         res = []
 
         def rec(i , subset):
 
-            if i >= len(nums):
+            if i == len(nums):
                 res.append(subset.copy())
                 return
-
+            
             subset.append(nums[i])
             rec(i + 1 , subset)
             subset.pop()
