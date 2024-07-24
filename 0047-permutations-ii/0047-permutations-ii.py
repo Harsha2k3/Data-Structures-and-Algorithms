1711c32e33c82d407c747603c0b36c04c2e102cc
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-07-24 17:11:22 +0530

    Time: 54 ms (56.83%), Space: 16.8 MB (66.04%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,31 +1,56 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        
-        # T.C = n! x n  (Combinations ==> n! and for loop ==> n)
-        # S.C = O(n) auxiliary S.C for recursion
 
         res = []
 
-        def rec(ind , nums):
-
+        def rec(ind, nums):
             if ind > len(nums) - 1:
                 res.append(nums.copy())
                 return
 
-            for i in range(ind , len(nums)):
-                if i > ind and nums[i] == nums[i - 1]:
+            seen = set()
+            for i in range(ind, len(nums)):
+                if nums[i] in seen:
                     continue
+                seen.add(nums[i])
                 
-                nums[ind] , nums[i] = nums[i] , nums[ind]
-                rec(ind + 1 , nums)
-                nums[ind] , nums[i] = nums[i] , nums[ind]
+                nums[ind], nums[i] = nums[i], nums[ind]
+                rec(ind + 1, nums)
+                nums[ind], nums[i] = nums[i], nums[ind]
 
-        nums.sort()
-        rec(0 , nums)
+        
+        rec(0, nums)
 
-        res_final = set()
+        return res
 
-        for i in res:
-            res_final.add(tuple(i))
 
-        return list(res_final)
+
+
+        # # T.C = n! x n  (Combinations ==> n! and for loop ==> n)
+        # # S.C = O(n) auxiliary S.C for recursion
+
+        # res = []
+
+        # def rec(ind , nums):
+
+        #     if ind > len(nums) - 1:
+        #         res.append(nums.copy())
+        #         return
+
+        #     for i in range(ind , len(nums)):
+        #         if i > ind and nums[i] == nums[i - 1]:
+        #             continue
+                
+        #         nums[ind] , nums[i] = nums[i] , nums[ind]
+        #         rec(ind + 1 , nums)
+        #         nums[ind] , nums[i] = nums[i] , nums[ind]
+
+        # nums.sort()
+        # rec(0 , nums)
+
+        # res_final = set()
+
+        # for i in res:
+        #     res_final.add(tuple(i))
+
+        # return list(res_final)
