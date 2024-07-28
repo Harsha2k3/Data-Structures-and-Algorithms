e4aea19c38abf2abbad0ac92f21e3fb11a604f66
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-07-28 21:31:23 +0530

    Time: 48 ms (49.2%), Space: 16.9 MB (24.38%) - LeetHub

--- 2416-evaluate-boolean-binary-tree/2416-evaluate-boolean-binary-tree.py
+++ 2416-evaluate-boolean-binary-tree/2416-evaluate-boolean-binary-tree.py
@@ -8,11 +8,11 @@ class Solution:
 
             if not root.left and not root.right:
 
-                if root.val == 0:
-                    return False
+                if root.val == 1:
+                    return True
 
                 else:
-                    return True
+                    return False
 
             l = dfs(root.left)
             r = dfs(root.right)
