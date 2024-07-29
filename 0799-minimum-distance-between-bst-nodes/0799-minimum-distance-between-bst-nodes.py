4ece65ca38032cea77ec9061fed7a157a7a3eef6
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-07-29 19:27:58 +0530

    Time: 41 ms (20.26%), Space: 16.5 MB (70.75%) - LeetHub

--- 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.py
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.py
@@ -1,6 +1,14 @@
 class Solution:
     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
 
+        # Consider a sorted array, [1 , 2 , 3 , 4]
+        # As it is sorted to find min difference between 2 items
+        # we can just find difference between adjacent elements
+        # i.e, 2 - 1 , 3 - 2 , 4 - 3
+        # Inorder traversal of a BST is a sorted array
+        # So apply the same here by doing an inorder traversal
+        # by tracking prev node and current node
+
         prev = None
         res = float("inf")
         
@@ -15,6 +23,7 @@ class Solution:
 
             if prev:
                 res = min(res , root.val - prev.val)
+                
             
             prev = root
 
