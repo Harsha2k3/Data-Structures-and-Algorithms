8dc7b3f28a13bc238b7f193bdf9c44936f02e7b3
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-08-09 18:58:48 +0530

    Time: 526 ms (41.52%), Space: 0B (100%) - LeetHub

--- 1161-project-employees-i/1161-project-employees-i.sql
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -2,3 +2,10 @@ select p.project_id , round(avg(e.experience_years) , 2) as average_years
 from Project as p
 left join Employee as e on p.employee_id = e.employee_id
 group by p.project_id
+
+-- (Or)
+
+-- select Project.project_id , round(avg(Employee.experience_years) , 2) as average_years
+-- from Project
+-- left join Employee on Project.employee_id = Employee.employee_id
+-- group by Project.project_id
