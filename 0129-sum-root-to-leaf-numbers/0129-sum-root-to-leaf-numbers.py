5c6a64c76aa19229c870771168c2e8d3fb1991d8
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-08-11 00:13:54 +0530

    Time: 31 ms (82.57%), Space: 16.5 MB (59.51%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -0,0 +1,29 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+
+        l = []
+        res = 0
+
+        def dfs(root):
+
+            nonlocal l , res
+
+            if not root:
+                return
+            
+            if not root.left and not root.right:
+                l.append(str(root.val))
+                res += int("".join(l))
+                return
+
+            l.append(str(root.val))
+            if root.left:
+                dfs(root.left)
+                l.pop()
+            if root.right:
+                dfs(root.right)
+                l.pop()
+        
+        dfs(root)
+
+        return res
