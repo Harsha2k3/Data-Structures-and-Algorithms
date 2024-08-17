58e196a51669acb902d2d8f97a62744bd1c7fc5f
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-08-17 12:51:17 +0530

    Time: 4803 ms (5.19%), Space: 47.4 MB (63.7%) - LeetHub

---
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+class Solution:
+    def minimumOperations(self, root: Optional[TreeNode]) -> int:
+        
+        res = 0
+
+        q = deque()
+
+        q.append(root)
+        
+        def bfs():
+
+            nonlocal res
+
+            while q:
+                level = []
+                for i in range(len(q)):
+
+                    node = q.popleft()
+                    level.append(node.val)
+
+                    if node.left:
+                        q.append(node.left)
+
+                    if node.right:
+                        q.append(node.right)
+
+                sorted_level = sorted(level)
+
+                swaps = 0
+
+                for i in range(len(level)):
+                    if level[i] != sorted_level[i]:
+                        j = level.index(sorted_level[i], i)
+                        level[i] , level[j] = level[j] , level[i]
+                        swaps += 1
+                
+                res += swaps
+        
+        bfs()
+
+        return res
