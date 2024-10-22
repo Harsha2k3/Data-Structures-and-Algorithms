2a1449956ab2ac41be58fe5e5509ffc5471e482a
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-10-22 12:06:05 +0530

    Time: 7 ms (99.37%), Space: 16.9 MB (6.92%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,22 +1,26 @@
 class Solution:
     def combinationSum(self, nums: List[int], t: int) -> List[List[int]]:
 
+        # Some what like take or not take pattern
+        # Use this pattern when,
+        # We are allowed to pick each number in input array more than once
+
         res = []
 
-        def rec(i , s , l):
+        def rec(i , t , l):
 
             if i == len(nums):
-                if s == 0:
+                if t == 0:
                     res.append(l.copy())
                     return
-            
-            else:
-                if nums[i] <= s:
-                    l.append(nums[i])
-                    rec(i , s - nums[i] , l)
-                    l.pop()
-                rec(i + 1 , s , l)
 
+            else:
+                if nums[i] <= t:
+                    l.append(nums[i])
+                    rec(i , t - nums[i] , l)
+                    l.pop()
+                rec(i + 1 , t , l)
+        
         rec(0 , t , [])
 
         return res
