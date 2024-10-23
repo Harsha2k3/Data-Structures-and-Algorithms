aa4cf513811ca3a4ebfca498b0667e6f9910a4b1
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-10-23 12:37:28 +0530

    Time: 3 ms (99.45%), Space: 17.9 MB (54.51%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.py
+++ 0800-letter-case-permutation/0800-letter-case-permutation.py
@@ -5,7 +5,8 @@ class Solution:
 
         res = []
 
-        def rec(ind, l):
+        def rec(ind , l):
+
             if ind == len(s):
                 res.append("".join(l))
                 return
@@ -14,24 +15,23 @@ class Solution:
                 rec(ind + 1, l)
 
             else:
-                # Recur without changing the case
-                rec(ind + 1, l)
 
-                # Recur after changing the case
-                if l[ind].islower():
-                    l[ind] = l[ind].upper()  
-                else:
-                    l[ind] = l[ind].lower()
-                    
-                rec(ind + 1, l)
+                rec(ind + 1 , l)
                 
-                # Backtrack
                 if l[ind].islower():
-                    l[ind] = l[ind].upper()  
+                    l[ind] = l[ind].upper()
+                    rec(ind + 1 , l)
 
                 else:
                     l[ind] = l[ind].lower()
+                    rec(ind + 1 , l)
+                
+                if l[ind].islower():
+                    l[ind] = l[ind].upper()
 
-        rec(0, list(s))
+                else:
+                    l[ind] = l[ind].lower()
+        
+        rec(0 , list(s))
 
         return res
