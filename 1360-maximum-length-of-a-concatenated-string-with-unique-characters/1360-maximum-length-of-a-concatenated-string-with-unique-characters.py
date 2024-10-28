105f0b001814a29346554112b4bde46737516ffd
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-10-28 19:35:19 +0530

    Time: 72 ms (42.75%), Space: 16.7 MB (37.28%) - LeetHub

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -2,21 +2,24 @@ class Solution:
     def maxLength(self, arr: List[str]) -> int:
 
         res = 0
-        
-        def rec(ind , l):
 
+        def rec(ind , l):
+            
             nonlocal res
             
             if len("".join(l)) == len("".join(arr)):
                 return
 
             for i in range(ind , len(arr)):
+                
                 l.append(arr[i])
+
                 if len("".join(l)) == len(set("".join(l))):
                     res = max(res , len("".join(l)))
                     rec(i + 1 , l)
+                
                 l.pop()
-
+        
         rec(0 , [])
 
-        return res 
+        return res
