235698bf22171181583d81a1a529ac918afddc70
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-11-02 20:57:14 +0530

    Time: 11 ms (71.34%), Space: 17 MB (86.94%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -6,97 +6,106 @@ class Solution:
     # This takes O(1) T.C for checking previous queens
     # We will use hashmap for achieving this
 
-    def solve(self, col, board, ans, leftrow, upperDiagonal, lowerDiagonal, n):
-        
-        if col == n:
-            ans.append(board[:])
-            return
-
-        for row in range(n):
-            if leftrow[row] == 0 and lowerDiagonal[row+col] == 0 and upperDiagonal[n-1+col-row] == 0:
-                board[row] = board[row][:col] + 'Q' + board[row][col+1:]
-                leftrow[row] = 1
-                lowerDiagonal[row+col] = 1
-                upperDiagonal[n-1+col-row] = 1
-                self.solve(col+1 , board, ans, leftrow,upperDiagonal, lowerDiagonal, n)
-                board[row] = board[row][:col] + '.' + board[row][col+1:]
-                leftrow[row] = 0
-                lowerDiagonal[row+col] = 0
-                upperDiagonal[n-1+col-row] = 0
-
-
     def solveNQueens(self, n: int) -> List[List[str]]:
-        ans = []
-        board = ['.'*n for _ in range(n)]
-        leftrow = [0]*n
-        upperDiagonal = [0]*(2*n-1)
-        lowerDiagonal = [0]*(2*n-1)
-        self.solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n)
-        return ans
+
+        res = []
+
+        board = ["." * n for i in range(n)]
+
+        lr = [0] * n                # Left row
+        ud = [0] * (2 * n - 1)      # Upper diagonal
+        ld = [0] * (2 * n - 1)      # Lower diagonal
 
 
+        def solve(col):
 
+            nonlocal res , board , lr , ud , ld
+
+            if col == n:
+                res.append(board.copy())
+                return
+
+            for row in range(n):
+                if lr[row] == 0 and ud[row + col] == 0 and ld[n - 1 + col - row] == 0:
+                    board[row] = board[row][:col] + "Q" + board[row][col + 1 : ]
+                    lr[row] = 1
+                    ud[row + col] = 1
+                    ld[n - 1 + col - row] = 1
+                    solve(col + 1)
+                    board[row] = board[row][:col] + "." + board[row][col + 1 : ]
+                    lr[row] = 0
+                    ud[row + col] = 0
+                    ld[n - 1 + col - row] = 0
+
+        solve(0)
+
+        return res
 
 
 
     # # This is not optimized
     # # Beacuse, we are taking extra O(n) T.C for checking previous queens (For isSafe function) 
 
-    # def isSafe(self, row, col, board, n):
-
-    #     # check upper element
-    #     duprow = row
-    #     dupcol = col
-
-    #     while row >= 0 and col >= 0:
-    #         if board[row][col] == 'Q':
-    #             return False
-    #         row -= 1
-    #         col -= 1
-
-
-    #     col = dupcol
-    #     row = duprow
-
-    #     while col >= 0:
-    #         if board[row][col] == 'Q':
-    #             return False
-    #         col -= 1
-
-
-    #     row = duprow
-    #     col = dupcol
-
-    #     while row < n and col >= 0:
-    #         if board[row][col] == 'Q':
-    #             return False
-    #         row += 1
-    #         col -= 1
-
-
-    #     return True
-
-
-    # def solve(self, col, board, res, n):
-
-    #     if col > n - 1:
-    #         res.append(list(board))
-    #         return
-
-
-    #     for row in range(n):
-    #         if self.isSafe(row , col , board , n):
-    #             board[row] = board[row][:col] + 'Q' + board[row][col+1:]
-    #             self.solve(col+1 , board , res , n)
-    #             board[row] = board[row][:col] + '.' + board[row][col+1:]
-
 
     # def solveNQueens(self, n: int) -> List[List[str]]:
 
     #     res = []
 
-    #     board = ['.' * n for i in range(n)]
+    #     board = ["." * n for i in range(n)]
 
-    #     self.solve(0, board, res, n)
+    #     def isSafe(row , col):
+
+    #         nonlocal res , board
+
+    #         duprow = row
+    #         dupcol = col
+
+    #         # Upper left diagonal
+
+    #         while row >= 0 and col >= 0:
+    #             if board[row][col] == "Q":
+    #                 return False
+    #             row -= 1
+    #             col -= 1
+
+    #         # Left row
+
+    #         row = duprow
+    #         col = dupcol
+            
+    #         while col >= 0:
+    #             if board[row][col] == "Q":
+    #                 return False
+    #             col -= 1
+
+    #         # Lower left diagonal
+
+    #         row = duprow
+    #         col = dupcol
+
+    #         while row <= n - 1 and col >= 0:
+    #             if board[row][col] == "Q":
+    #                 return False
+    #             row += 1
+    #             col -= 1
+
+    #         return True
+
+
+    #     def solve(col):
+
+    #         nonlocal res , board
+
+    #         if col == n:
+    #             res.append(board.copy())
+    #             return
+
+    #         for row in range(n):
+    #             if isSafe(row , col):
+    #                 board[row] = board[row][:col] + "Q" + board[row][col + 1 : ]
+    #                 solve(col + 1)
+    #                 board[row] = board[row][:col] + "." + board[row][col + 1 : ]
+
+    #     solve(0)
 
     #     return res
