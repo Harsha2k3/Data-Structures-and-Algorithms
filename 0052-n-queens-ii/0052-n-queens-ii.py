9d32623d812334424f28ae60d2e3e6a7d20768e1
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-11-03 10:59:42 +0530

    Time: 8 ms (70.36%), Space: 16.6 MB (36.25%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,30 +1,31 @@
 class Solution:
-    def solve(self, col, leftrow, upperDiagonal, lowerDiagonal, n):
-        
-        if col > n - 1:
-            return 1
-        # else:
-        #     return 0
+    def totalNQueens(self, n: int) -> int:
 
         count = 0
 
-        for row in range(n):
-            if leftrow[row] == 0 and lowerDiagonal[row+col] == 0 and upperDiagonal[n-1+col-row] == 0:
-                leftrow[row] = 1
-                lowerDiagonal[row+col] = 1
-                upperDiagonal[n-1+col-row] = 1
-                count += self.solve(col+1 , leftrow,upperDiagonal, lowerDiagonal, n)
-                leftrow[row] = 0
-                lowerDiagonal[row+col] = 0
-                upperDiagonal[n-1+col-row] = 0
+        lr = [0] * n                # Left row
+        ud = [0] * (2 * n - 1)      # Upper diagonal
+        ld = [0] * (2 * n - 1)      # Lower diagonal
+
+
+        def solve(col):
+
+            nonlocal count , lr , ud , ld
+
+            if col == n:
+                count += 1
+                return
+
+            for row in range(n):
+                if lr[row] == 0 and ud[row + col] == 0 and ld[n - 1 + col - row] == 0:
+                    lr[row] = 1
+                    ud[row + col] = 1
+                    ld[n - 1 + col - row] = 1
+                    solve(col + 1)
+                    lr[row] = 0
+                    ud[row + col] = 0
+                    ld[n - 1 + col - row] = 0
+
+        solve(0)
 
         return count
-
-
-    def totalNQueens(self, n: int) -> int:
-        
-        leftrow = [0] * n
-        upperDiagonal = [0] * (2 * n - 1)
-        lowerDiagonal = [0] * (2 * n - 1)
-
-        return self.solve(0, leftrow, upperDiagonal, lowerDiagonal, n)
