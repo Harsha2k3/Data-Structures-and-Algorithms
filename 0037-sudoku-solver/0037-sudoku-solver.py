ef21052c7d66e2a96dad838b6695579875d2efbf
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-11-06 11:45:21 +0530

    Time: 278 ms (37.59%), Space: 16.6 MB (85.87%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,28 +1,39 @@
 class Solution:
-    
-    def isValid(self , board: List[List[str]], row: int, col: int, c: str) -> bool:
-        for i in range(9):
-            if board[i][col] == c:
-                return False
-            if board[row][i] == c:
-                return False
-            if board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == c:
-                return False
-        return True
+    def solveSudoku(self, board: List[List[str]]) -> None:
 
+        def isValid(row , col , c):
 
+            for i in range(9):
 
-
-    def solveSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == ".":
-                    for c in "123456789":
-                        if self.isValid(board, i, j, c):
-                            board[i][j] = c
-                            if self.solveSudoku(board):
-                                return True
-                            else:
-                                board[i][j] = "."
+                if board[i][col] == c:
                     return False
-        return True    
+                
+                if board[row][i] == c:
+                    return False
+                
+            
+            start_row , start_col = 3 * (row // 3) , 3 * (col // 3)
+
+            for i in range(3):
+                for j in range(3):
+                    if board[start_row + i][start_col + j] == c:
+                        return False
+            
+            return True
+
+        def rec():
+
+            for i in range(len(board)):
+                for j in range(len(board[0])):
+                    if board[i][j] == ".":
+                        for c in "123456789":
+                            if isValid(i , j , c):
+                                board[i][j] = c
+                                if rec():
+                                    return True
+                                else:
+                                    board[i][j] = "."
+                        return False
+            return True
+
+        rec()
