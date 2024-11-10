7af8e3ea16bffd2c6e1a6fee2054a0a82c4c7486
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-11-10 11:40:23 +0530

    Time: 59 ms (31.64%), Space: 29.5 MB (6.33%) - LeetHub

--- 1120-flower-planting-with-no-adjacent/1120-flower-planting-with-no-adjacent.py
+++ 1120-flower-planting-with-no-adjacent/1120-flower-planting-with-no-adjacent.py
@@ -1,45 +1,52 @@
+
+
+
+
+
+
 class Solution:
+    def gardenNoAdj(self, n: int, paths: List[List[int]]) -> List[int]:
 
-    # Same as m - coloring problem in backtracking
-
-    def isSafe(self, node, color, graph, col):
-
-        for neighbor in graph[node]:
-            if color[neighbor] == col:
-                return False
-        return True
-
-    def solve(self, node, color, m, N, graph):
-
-        if node == N + 1:
-            return True
-
-        for i in range(1, m + 1):
-            if self.isSafe(node, color, graph, i):
-                color[node] = i
-
-                if self.solve(node + 1, color, m, N, graph):
-                    return True
-
-                color[node] = 0
-
-        return False
-
-    def gardenNoAdj(self, N: int, paths: List[List[int]]) -> List[int]:
-        
         # Create adjacency list for the graph
-        graph = [[0 , 0]] + [[] for _ in range(N)]
+        graph = [[0 , 0]] + [[] for _ in range(n)]
 
         for u, v in paths:
             graph[u].append(v)
             graph[v].append(u)
+
+        # graph = [[0, 0], [2, 3], [1, 3], [2, 1]]
+        # Neighbours of node 1 ==> 2 , 3
+        # Neighbours of node 2 ==> 1 , 3
+        # Neighbours of node 3 ==> 2 , 1
         
         print(graph)
 
-        color = [0] * (N + 1)
+        color = [0] * (n + 1)
+
         m = 4
 
-        if self.solve(1, color, m, N, graph):
-            return color[1:]
-        else:
-            return []
+        def isSafe(node , col):
+
+            for n in graph[node]:
+                if color[n] == col:
+                    return False
+            return True
+
+        def rec(node):
+
+            nonlocal graph , color , m , n
+
+            if node == n + 1:
+                return True
+
+            for i in range(1 , m + 1):
+                if isSafe(node , i):
+                    color[node] = i
+                    if rec(node + 1):
+                        return True
+                    color[node] = 0
+            return False
+
+        rec(1)
+
+        return color[1:]
