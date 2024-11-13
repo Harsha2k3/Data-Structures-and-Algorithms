993ecd61c31b25dc9157777836178129bf14314b
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-11-13 12:10:27 +0530

    Time: 8 ms (94.64%), Space: 22.1 MB (30.5%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -4,28 +4,22 @@ class Solution:
         # Mix of both 104. Maximum Depth of Binary Tree
         # and 543. Diameter of Binary Tree
 
-        mps = float("-inf")     # Max path sum
+        mps = float("-inf")
 
-        def rec(root):
+        def dfs(root):
 
             nonlocal mps
 
             if not root:
                 return 0
-
-            mls = max(0 , rec(root.left))     # Max left sum
-            mrs = max(0 , rec(root.right))    # Max right sum
-
-            # Here we do mls = max(0 , rec(root.left)) and mrs = max(0 , rec(root.right))
-            # not mls = max(mls , rec(root.left)) and mrs = max(mrs , rec(root.right))
-            # Because we should not consider -ve values
-            # https://www.youtube.com/watch?v=WszrfSwMz58&list=PLkjdNRgDmcc0Pom5erUBU4ZayeU9AyRRu&index=19
-            # Watch from 13:24 to 14:41
+            
+            mls = max(0 , dfs(root.left))
+            mrs = max(0 , dfs(root.right))
 
             mps = max(mps , mls + root.val + mrs)
 
             return root.val + max(mls , mrs)
 
-        rec(root)
+        dfs(root)
 
         return mps
