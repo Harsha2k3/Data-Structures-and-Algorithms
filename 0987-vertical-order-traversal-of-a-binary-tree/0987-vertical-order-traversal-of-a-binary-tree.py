34aed84eb3308683cea733af7438e7b8ab6ae291
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-11-17 21:20:02 +0530

    Time: 0 ms (100%), Space: 17 MB (37.97%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,34 @@
+from collections import defaultdict , deque
+
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+
+        # Just mark vertical and level by traversing through BFS 
+
+        node_list = []
+
+        q = deque([(root , 0 , 0)])    # root , vertical , level
+
+        while q:
+            for i in range(len(q)):
+                node , v , l = q.popleft()
+                node_list.append((v , l , node.val))
+                if node.left:
+                    q.append((node.left , v - 1 , l + 1))
+                if node.right:
+                    q.append((node.right , v + 1 , l + 1))
+
+        print(node_list)
+        
+        node_list.sort()
+
+        print(node_list)
+
+        res = defaultdict(list)
+
+        for v , l , node in node_list:
+            res[v].append(node)
+
+        print(res)
+        
+        return [res[i] for i in sorted(res.keys())]
