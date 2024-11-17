f979c49b494eaca1d65e22bdda7252917f5cdaea
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-11-17 12:08:48 +0530

    Time: 3 ms (100%), Space: 16.7 MB (28.01%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -5,11 +5,10 @@ class Solution:
 
             if not root1 and not root2:
                 return True
-            
+
             if not root1 or not root2:
                 return False
-
-            # Simultaneously we have to travserse the trees
+            
             return root1.val == root2.val and rec(root1.left , root2.left) and rec(root1.right , root2.right)
-
-        return rec(p , q)   
+        
+        return rec(p , q)
