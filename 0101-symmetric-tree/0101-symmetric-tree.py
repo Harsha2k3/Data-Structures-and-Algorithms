d6dead39dcd33c3b69489b261fe882e22874927b
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-11-19 22:33:18 +0530

    Time: 3 ms (9.48%), Space: 16.9 MB (18.23%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -1,33 +1,19 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 
-        # https://www.youtube.com/watch?v=nKggNAiEpBE&list=PLkjdNRgDmcc0Pom5erUBU4ZayeU9AyRRu&index=26
-        # Watch from 2:24 to 6:00
+        res = True
+        
+        root1 = root.left
+        root2 = root.right
 
-        root1 , root2 = root.left , root.right
+        def dfs(root1 , root2):
 
-        def rec(root1 , root2):
-
+            if not root1 and not root2:
+                return True
+            
             if not root1 or not root2:
-                return root1 == root2
-
-            if root1.val != root2.val:
                 return False
-
-            # root1 , root2 = root1.left , root2.right
-
-            # if rec(root1 , root2):
-            #     return True
-            # return False
-
-            # # Backtracking
-            # root1 , root2 = root1.right , root2.left
-
-            # if rec(root1 , root2):
-            #     return True
-            # return False
-
-            return rec(root1.left , root2.right) and rec(root1.right , root2.left)
-            # rec(root1.right , root2.left) ==> Backtracking
-
-        return rec(root1 , root2)
+            
+            return root1.val == root2.val and dfs(root1.left , root2.right) and dfs(root1.right , root2.left)
+        
+        return dfs(root1 , root2)
