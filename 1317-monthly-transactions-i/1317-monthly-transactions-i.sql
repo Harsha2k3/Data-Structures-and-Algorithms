3d6d81d400b793ef5c1fb2f9da6eac836e066570
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2024-12-15 14:24:55 +0530

    Time: 554 ms (91.59%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,3 +1,12 @@
-select  substr(trans_date , 1 , 7) as month , country , count(id) as trans_count , sum(case when state = 'approved' then 1 else 0 end) as approved_count , sum(amount) as trans_total_amount , sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+select substr(trans_date , 1 , 7) as month,
+country,
+count(*) as trans_count, 
+sum(case when state = "approved" then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state = "approved" then amount else 0 end) as approved_total_amount
 from Transactions
-group by month , country
+group by month , country;
+
+
+
+
