dfbc32687cd5800bda913537506ef185353bddfc
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-01-23 19:54:18 +0530

    Time: 4 ms (12.69%), Space: 18.1 MB (9.19%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -6,48 +6,48 @@ class Solution:
         # T.C = n! x n  (Combinations ==> n! and for loop ==> n)
         # S.C = O(n) auxiliary S.C for recursion
 
-        # res = []
+        res = []
 
-        # def rec(ind , nums):
+        def rec(ind , nums):
 
-        #     nonlocal res
-            
-        #     if ind == len(nums):
-        #         res.append(nums.copy())
-        #         return
+            nonlocal res
 
-        #     for i in range(ind , len(nums)):
-        #         nums[i] , nums[ind] = nums[ind] , nums[i]
-        #         rec(ind + 1 , nums)
-        #         nums[i] , nums[ind] = nums[ind] , nums[i]
+            if ind == len(nums):
+                res.append(nums.copy())
+                return
+
+            for i in range(ind , len(nums)):
+                nums[ind] , nums[i] = nums[i] , nums[ind]
+                rec(ind + 1 , nums)
+                nums[ind] , nums[i] = nums[i] , nums[ind]
         
-        # rec(0 , nums)
+        rec(0 , nums)
 
-        # return res
+        return res
 
 
         # # Takes extra memory for hashmap array
         # # T.C = n! x n  (Combinations ==> n! and for loop ==> n)
         # # S.C = O(n) normal S.C and O(n) auxiliary S.C for recursion
         
-        res = []
+        # res = []
 
-        def rec(l , h):
+        # def rec(l , h):
 
-            nonlocal res
+        #     nonlocal res
 
-            if len(l) == len(nums):
-                res.append(l.copy())
-                return
+        #     if len(l) == len(nums):
+        #         res.append(l.copy())
+        #         return
 
-            for i in range(len(h)):
-                if not h[i]:
-                    l.append(nums[i])
-                    h[i] = 1
-                    rec(l , h)
-                    l.pop()
-                    h[i] = 0
+        #     for i in range(len(h)):
+        #         if not h[i]:
+        #             l.append(nums[i])
+        #             h[i] = 1
+        #             rec(l , h)
+        #             l.pop()
+        #             h[i] = 0
         
-        rec([] , [0] * len(nums))
+        # rec([] , [0] * len(nums))
 
-        return res
+        # return res
