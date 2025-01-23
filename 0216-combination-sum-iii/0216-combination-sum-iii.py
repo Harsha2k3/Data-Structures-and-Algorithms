a57b7bfb89b6b6870bba6d80bf6273590882d27a
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-01-23 19:00:24 +0530

    Time: 0 ms (100%), Space: 17.6 MB (56.31%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -2,19 +2,29 @@ class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         
         nums = [i for i in range(1 , 10)]
+
         res = []
 
-        def rec(l , ind , t):
+        def rec(ind , t , l):
 
-            if len(l) == k and t == 0:
+            nonlocal res
+            
+            if t == 0 and len(l) == k:
                 res.append(l.copy())
+                return
+                
+            if ind == len(nums):
                 return
 
             for i in range(ind , len(nums)):
+
+                if t < nums[i]:
+                    break
+
                 l.append(nums[i])
-                rec(l , i + 1 , t - nums[i])
+                rec(i + 1 , t - nums[i] , l)
                 l.pop()
         
-        rec([] , 0 , n)
+        rec(0 , n , [])
 
         return res
