af76063ebcce5fce3196c65eaabdc557f6b70dc8
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-01-26 21:25:58 +0530

    Time: 0 ms (100%), Space: 17.9 MB (49.47%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -5,13 +5,15 @@ class Solution:
 
         def rec(s , open_ , close):
 
+            nonlocal res
+
             if len(s) == 2 * n:
                 res.append(s)
                 return
-            
+
             if open_ < n:
                 rec(s + "(" , open_ + 1 , close)
-
+            
             if close < open_:
                 rec(s + ")" , open_ , close + 1)
         
