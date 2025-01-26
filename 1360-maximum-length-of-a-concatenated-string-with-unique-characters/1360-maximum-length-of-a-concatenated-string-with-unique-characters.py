f44202dbf2ab2bcbbcadef615a85e915a6b86048
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-01-26 22:20:52 +0530

    Time: 70 ms (38.78%), Space: 17.9 MB (48.88%) - LeetHub

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -4,14 +4,14 @@ class Solution:
         res = 0
 
         def rec(ind , l):
-            
+
             nonlocal res
-            
-            if len("".join(l)) == len("".join(arr)):
+
+            if "".join(l) == "".join(arr):
                 return
 
             for i in range(ind , len(arr)):
-                
+
                 l.append(arr[i])
 
                 if len("".join(l)) == len(set("".join(l))):
@@ -19,7 +19,7 @@ class Solution:
                     rec(i + 1 , l)
                 
                 l.pop()
-        
+            
         rec(0 , [])
 
         return res
