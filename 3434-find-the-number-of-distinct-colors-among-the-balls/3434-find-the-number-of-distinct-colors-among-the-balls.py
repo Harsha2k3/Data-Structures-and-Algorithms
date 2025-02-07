af1e60d1fdac8b858a3fa9336e8d40797baec571
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-02-08 00:41:23 +0530

    Time: 92 ms (29.74%), Space: 68.1 MB (22.25%) - LeetHub

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.py
@@ -0,0 +1,29 @@
+from collections import defaultdict 
+
+class Solution:
+    def queryResults(self, limit: int, q: List[List[int]]) -> List[int]:
+        
+        count = 0
+        res = []
+        s = set()
+
+        colors = {}  
+        color_count = defaultdict(int)  
+
+        for i in q:
+            temp = colors.get(i[0] , -1)
+            colors[i[0]] = i[1]
+            if temp != -1:
+                color_count[temp] -= 1  
+                if color_count[temp] == 0:
+                    s.remove(temp)  
+
+            color_count[i[1]] += 1  
+            
+            if i[1] not in s:
+                s.add(i[1])
+                count += 1
+            
+            res.append(len(s))
+
+        return res
