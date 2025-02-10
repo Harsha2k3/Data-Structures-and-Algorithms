fbaf27981d369681f9edff0ce2838e459b8977f5
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-02-10 21:01:07 +0530

    Time: 9 ms (5.18%), Space: 17.7 MB (43.55%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.py
@@ -0,0 +1,21 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        
+        l = []
+        ind = []
+        rind = []
+
+        for i in range(len(s)):
+            
+            if s[i].isalpha():
+                ind.append(i)
+            
+            else:
+                rind.append(ind.pop())
+                rind.append(i)
+
+        rind.sort()
+
+        s = list(s)
+        
+        return "".join([num for i , num in enumerate(s) if i not in set(rind)])
