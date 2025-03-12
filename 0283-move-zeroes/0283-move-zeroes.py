55c9a6bd4b41e1618a5ed53f0231840430986bc7
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-03-12 18:18:09 +0530

    Time: 6 ms (50.73%), Space: 18.8 MB (49.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,21 +1,21 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
 
-        count = 0
-        j = 0
-        l = []
-        k = -1
+        # 2 ptr's
 
-        for i in range(len(nums)):
-            if(nums[i] == 0):
-                l.append(nums[i])
-                count += 1
-            else:
-                nums[j] = nums[i]
-                j += 1
+        l = 0
+        r = 0
 
-        for i in range(count):
-            nums.pop(k)
+        while r < len(nums):
+            if nums[r]:
+                nums[l] , nums[r] = nums[r] , nums[l]
+                l += 1
+            r += 1
+        
+        return nums
 
-        for i in range(count):
-            nums.append(0)
+        # [0,1,0,3,12]
+        # [1,0,0,3,12]
+        # [1,0,0,3,12]
+        # [1,3,0,0,12]
+        # [1,3,12,0,0]
