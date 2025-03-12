d3893106943c7471399f712d7ed01b6883b52602
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-03-12 18:05:49 +0530

    Time: 10 ms (50.24%), Space: 14 MB (16.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,25 @@
 class Solution(object):
     def twoSum(self, nums, t):
 
-        l = []
-        k = 1
+        d = {}
 
-        for i in range(len(nums)):
-            for j in range(k,len(nums)):
-                if(nums[i]+nums[j] == t):
-                    l.append(i)
-                    l.append(j)
-                    break
-            k+=1
-        
-        return l
+        # Store indices as lists to handle duplicates
+        for i , num in enumerate(nums):
+            if num in d:
+                d[num].append(i)
+            else:
+                d[num] = [i]
+
+        nums.sort()
+
+        l , r = 0 , len(nums) - 1
+
+        while l < r:
+            if nums[l] + nums[r] == t:
+                return [d[nums[l]].pop(0) , d[nums[r]].pop(0)]
+            
+            elif nums[l] + nums[r] > t:
+                r -= 1
+            
+            else:
+                l += 1
