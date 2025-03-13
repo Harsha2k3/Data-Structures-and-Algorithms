6ca120b118e91e76ed93563bdabe923258ed588a
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-03-13 15:48:37 +0530

    Time: 83 ms (54.52%), Space: 18.2 MB (17.05%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,41 +1,64 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        vowels = set(["a", "e", "i", "o", "u"])
 
-        curr_arr = s[:k]
-        max_count = 0
+        v = "aeiou"
+
+        start = 0
         count = 0
 
-        for char in curr_arr:
-            if char in vowels:
-                count += 1
+        res = float("-inf")
 
-        max_count = count
+        for end in range(len(s)):
+            if s[end] in v: count += 1 
 
-        for i in range(1, len(s) - k + 1):
-            if s[i - 1] in vowels:
-                count -= 1
-            if s[i + k - 1] in vowels:
-                count += 1
+            if end - start + 1 == k:
+                res = max(res , count)
+                if s[start] in v: count -= 1 
+                start += 1
 
-            max_count = max(max_count, count)
-            curr_arr = s[i : k + i]
-
-        return max_count
-
-
-# In each iteration, check if the character at index i - 1 (the character leaving the window) is a vowel. If it is, decrement the count by 1 since it's no longer in the current substring.
+        return res 
 
 
 
 
-# Initialize the current window to s[:k]
-# Count the number of vowels in s[:k] and store it in count
-# Assign that count in max_count initially
-# Now move the window using a for loop
-# In each ireration,
-# s[i-1] leaves the window
-# s[i+k-1] eneters the window
-# As s[i-1] leaves the window,if s[i-1] is vowel the count -= 1
-# As s[i+k-1] enters the window,if s[i+k-1] is vowel the count += 1
-# In each iteration max_count = max(max_count,count)
+
+
+#         vowels = set(["a", "e", "i", "o", "u"])
+
+#         curr_arr = s[:k]
+#         max_count = 0
+#         count = 0
+
+#         for char in curr_arr:
+#             if char in vowels:
+#                 count += 1
+
+#         max_count = count
+
+#         for i in range(1, len(s) - k + 1):
+#             if s[i - 1] in vowels:
+#                 count -= 1
+#             if s[i + k - 1] in vowels:
+#                 count += 1
+
+#             max_count = max(max_count, count)
+#             curr_arr = s[i : k + i]
+
+#         return max_count
+
+
+# # In each iteration, check if the character at index i - 1 (the character leaving the window) is a vowel. If it is, decrement the count by 1 since it's no longer in the current substring.
+
+
+
+
+# # Initialize the current window to s[:k]
+# # Count the number of vowels in s[:k] and store it in count
+# # Assign that count in max_count initially
+# # Now move the window using a for loop
+# # In each ireration,
+# # s[i-1] leaves the window
+# # s[i+k-1] eneters the window
+# # As s[i-1] leaves the window,if s[i-1] is vowel the count -= 1
+# # As s[i+k-1] enters the window,if s[i+k-1] is vowel the count += 1
+# # In each iteration max_count = max(max_count,count)
