29ada9dddb2c05ba0e5c43edc1bd0d36f3bf2d6f
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-03-14 15:43:24 +0530

    Time: 0 ms (100%), Space: 17.7 MB (61.97%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,26 +1,101 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-        dummy = ListNode(0)  
-        temp = dummy  
-        
+        dummy = ListNode(0)
+        temp = dummy
+
         while list1 and list2:
+            
             if list1.val <= list2.val:
                 temp.next = list1
                 list1 = list1.next
+
             else:
                 temp.next = list2
                 list2 = list2.next
-
-            # We update list1 and list2 based on which value is
-            # less among list1.val and list2.val to keep the 
-            # resultant list sorted
+            
             temp = temp.next
-        
-        # Attach the remaining nodes, if any, from list1 or list2
+            
         if list1:
             temp.next = list1
-        if list2:
+        else:
             temp.next = list2
+            
+        return dummy.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # dummy = ListNode(0)  
+        # temp = dummy  
         
-        return dummy.next  # Return the head of the merged list
+        # while list1 and list2:
+        #     if list1.val <= list2.val:
+        #         temp.next = list1
+        #         list1 = list1.next
+        #     else:
+        #         temp.next = list2
+        #         list2 = list2.next
+
+        #     # We update list1 and list2 based on which value is
+        #     # less among list1.val and list2.val to keep the 
+        #     # resultant list sorted
+        #     temp = temp.next
+        
+        # # Attach the remaining nodes, if any, from list1 or list2
+        # if list1:
+        #     temp.next = list1
+        # if list2:
+        #     temp.next = list2
+        
+        # return dummy.next  # Return the head of the merged list
+
+
+
+
+
+
+        # dummy = ListNode(0)
+        # temp = dummy
+
+        # def rec():
+
+        #     nonlocal dummy,temp,list1,list2
+
+        #     if((not list1) or (not list2)):
+            
+        #         if(list1):
+        #             temp.next = list1
+                
+        #         if(list2):
+        #             temp.next = list2
+                
+        #         return dummy.next
+
+        #     else:
+
+        #         if(list1.val <= list2.val):
+        #             temp.next = list1
+        #             list1 = list1.next
+                
+        #         else:
+        #             temp.next = list2
+        #             list2 = list2.next
+
+        #         temp = temp.next
+
+        #         return rec()
+
+        # return rec()
