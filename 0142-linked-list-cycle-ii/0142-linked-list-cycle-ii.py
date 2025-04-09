9f122a867a0274809da8ede1ece0416e2af26bb8
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-04-09 23:25:04 +0530

    Time: 49 ms (43.42%), Space: 19.5 MB (95.25%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -1,21 +1,38 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
+        # slow = head
+        # fast = head
+
+        # while(fast and fast.next):
+        #     slow = slow.next
+        #     fast = fast.next.next
+
+        #     if slow == fast:
+        #         slow = head
+        #         # If the pointers meet, there is a cycle in the linked list.
+        #         # Reset the slow pointer to the head of the linked list, and move both pointers one step at a time
+        #         # until they meet again. The node where they meet is the starting point of the cycle.
+        #         while(slow != fast):
+        #             slow = slow.next
+        #             fast = fast.next
+        #         return slow   # or return fast
+
+        # return None
+
+
         slow = head
         fast = head
 
-        while(fast and fast.next):
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
             if slow == fast:
                 slow = head
-                # If the pointers meet, there is a cycle in the linked list.
-                # Reset the slow pointer to the head of the linked list, and move both pointers one step at a time
-                # until they meet again. The node where they meet is the starting point of the cycle.
-                while(slow != fast):
+                while slow != fast:
                     slow = slow.next
                     fast = fast.next
-                return slow   # or return fast
-
+                return slow
+        
         return None
