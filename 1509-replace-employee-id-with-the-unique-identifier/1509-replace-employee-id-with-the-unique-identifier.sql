fd7b5a3a623d690daee1b271609610db2d9c3c77
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-04-24 20:02:29 +0530

    Time: 1129 ms (90.13%), Space: 0B (100%) - LeetHub

--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,4 @@
-select EmployeeUNI.unique_id , Employees.name
-from Employees
-left join EmployeeUNI on EmployeeUNI.id = Employees.id;
+select eu.unique_id, e.name
+from Employees as e
+left join EmployeeUNI as eu
+on e.id = eu.id;
