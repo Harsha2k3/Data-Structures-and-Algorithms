fda3bfbd38823646b57347580d0653f6294cf26a
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-04-24 19:28:04 +0530

    Time: 1295 ms (13.33%), Space: 0B (100%) - LeetHub

--- 1161-project-employees-i/1161-project-employees-i.sql
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -1,4 +1,14 @@
-select p.project_id , round(avg(e.experience_years) , 2) as average_years 
-from Project as p
-left join Employee as e on p.employee_id = e.employee_id
+select p.project_id, round(avg(e.experience_years), 2) as average_years
+from Employee as e
+left join Project as p
+on p.employee_id = e.employee_id
+where p.employee_id is not Null
 group by p.project_id;
+
+
+
+-- Execute this and see for clarity
+-- select *
+-- from Employee as e
+-- left join Project as p
+-- on p.employee_id = e.employee_id;
