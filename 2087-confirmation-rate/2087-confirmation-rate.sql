78dbf1e285f01e3378e5b292c7d73639463f83de
Harsha <89353121+Harsha2k3@users.noreply.github.com>
2025-05-04 19:53:21 +0530

    Time: 715 ms (55.53%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,4 +1,17 @@
-select sig.user_id , round(sum(case when action = "confirmed" then 1 else 0 end) / count(*) , 2) as confirmation_rate
-from Signups as sig
-left join Confirmations as con on sig.user_id = con.user_id
-group by sig.user_id
+select s.user_id , round(sum(case when c.action = "confirmed" then 1 else 0 end) / count(*), 2) as confirmation_rate 
+from Signups s
+left join Confirmations c on s.user_id = c.user_id
+group by s.user_id;
+
+
+# Why it is not c.user_id?
+
+# We want every user from the Signups table in the result — even 
+# if they never made a confirmation request.
+# if we used GROUP BY c.user_id, users with no confirmations 
+# (like user 6) would be excluded.
+
+#  Run this and see for clarity
+-- select *
+-- from Signups s
+-- left join Confirmations c on s.user_id = c.user_id;
